6ed89eb6d90c672c97ee0c957f3e3ae4a9f974cb
Mastel22 <52410156+Mastel22@users.noreply.github.com>
2019-09-01 21:48:45 +0200

    ft(As a mentor I should be able to accept a mentorship session)-added the related codes for a mentor to be able to accept a mentorship session request[ Finishes #168237557 ]

--- server/db/data.js
+++ server/db/data.js
@@ -3,8 +3,8 @@ import Session from '../models/session.model';
 
 export const users = [
   new User(1, 'Pierrette', 'Mastel', 'pmastel82@gmail.com', '$2b$10$t1F0QNy0WuM00BQiJmPApulsLAnWBm4DYNo3f/JOHr44Vn5zXyZ7m', 'KK 183', '5years of web developments', 'Web Developer', '3years developing apps', 'admin'),
-  new User(2, 'Jean Luc', 'MASTER', 'master@gmail.com', '$2b$10$t1F0QNy0WuM00BQiJmPApulsLAnWBm4DYNo3f/JOHr44Vn5zXyZ7m', 'KK 183', '5years of web developments', 'Web Developer', '3years developing apps', 'mentor'),
-]; // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBtYXN0ZWw4MkBnbWFpbC5jb20iLCJpYXQiOjE1NjczNTQ1MjB9.VmHo51vWcNY3jTB3GQCwLKDMqRSROSKJNjAQcwblCJQ
+  new User(2, 'Jean Luc', 'MASTER', 'master@gmail.com', '$2b$10$XtkjKPRXPMoAGzoUFlvSIOeQlxw3XCF6HoQHIVo0e.AolkXlvr/QG', 'KK 183', '5years of web developments', 'Web Developer', '3years developing apps', 'mentor'),
+];
 
 export const sessions = [
   new Session(1, 2, 2, 'Would you be my mentor', 'pmastel82@gmail.com'),
